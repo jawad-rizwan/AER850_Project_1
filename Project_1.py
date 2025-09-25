340a51626982b07b10d6aab4ebd6e25284a048a2
jawad-rizwan <jawad.rizwan117@gmail.com>
2025-09-25 15:54:46 -0400

    feat: Add Linux compat, display stat data and plot histogram

--- Project_1.py
+++ Project_1.py
@@ -1,3 +1,18 @@
-# Importing the data
+# Importing necessary libraries
+import os
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# Forcing the use of xcb platform for Qt (I'm using Linux and wayland doesn't seem to work for me)
+os.environ["QT_QPA_PLATFORM"] = "xcb"
+
+# Importing the data
 data = pd.read_csv("Project 1 Data.csv")
+
+# Displaying statistical summary of data frame
+print(data.describe())
+
+# Plotting histograms for each column in the data frame
+data.hist()
+plt.show()
